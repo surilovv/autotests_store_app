681014ac9704a707cca0035b75d51fa4807f0ee5
surilovv <131250284+surilovv@users.noreply.github.com>
2024-03-29 14:06:26 +0300

    Update test_stores.py

--- tests/test_stores.py
+++ tests/test_stores.py
@@ -1,3 +1,14 @@
+import allure
+
 from env_config import ExpectedData
 def test_get_store(stores_route):
     stores_route.get_store(ExpectedData.STORE,1)
+
+@allure.description("получение объекта магазин по id")
+@allure.title("Get store")
+@allure.label("owner", "Dima")
+@allure.severity(allure.severity_level.BLOCKER)
+@allure.suite("E2E")
+def test_1():
+    with allure.step("step1"):
+        assert 1 == 2
